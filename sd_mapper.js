57570863a4d184189af3ed110a727905644205a3
Gabriel Torres <gtorres043@west-mec.org>
2019-02-12 18:23:27 -0700

    Finished code
    needs comments

---
+++ sd_mapper.js
@@ -0,0 +1,21 @@
+"use strict";
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Case Problem 1
+
+   Planisphere Script
+   Author: Gabriel Torres
+   Date:   2/12/19
+
+*/
+
+var thisTime = new Date();
+var timeStr = thisTime.toLocaleString();
+document.getElementById("timeStamp").innerHTML = timeStr;
+var thisHour = thisTime.getHours();
+var thisMonth = thisTime.getMonth();
+
+var mapNum = ((2 * thisMonth + thisHour) % 24);
+var imgStr = "<img src='sd_sky" + mapNum + ".png' />";
+document.getElementById("planisphere").insertAdjacentHTML('afterbegin', imgStr);

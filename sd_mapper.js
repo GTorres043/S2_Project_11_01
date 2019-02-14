f9fc0fa177a1c642accf41b116cecba5e67576da
Gabriel Torres <gtorres043@west-mec.org>
2019-02-13 17:15:11 -0700

    finished code
    ready for review

--- sd_mapper.js
+++ sd_mapper.js
@@ -10,12 +10,16 @@
 
 */
 
+//this will start the time in the page to display the present time
 var thisTime = new Date();
 var timeStr = thisTime.toLocaleString();
 document.getElementById("timeStamp").innerHTML = timeStr;
+
+//will display the image depending on the date 
 var thisHour = thisTime.getHours();
 var thisMonth = thisTime.getMonth();
 
+//this is the formula to choose which image is going to be displayed depeneding on the date
 var mapNum = ((2 * thisMonth + thisHour) % 24);
 var imgStr = "<img src='sd_sky" + mapNum + ".png' />";
 document.getElementById("planisphere").insertAdjacentHTML('afterbegin', imgStr);
